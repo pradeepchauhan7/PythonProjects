2e089dd9cee78fbb8feda41038399d0b9a3e1908
pradeep <pradeep@relinns.com>
2018-05-04 11:14:42 +0530

    new updates

--- webscraping/practice2.py
+++ webscraping/practice2.py
@@ -1,11 +1,8 @@
 from bs4 import BeautifulSoup
 import re
 from urllib.request import urlopen, Request
-import json
-import requests
 
-print("Enter the file path")
-file = input()
+file = input("Enter the file path :\n")
 with open(file) as fp:
    line = fp.readline()
    while line:
@@ -15,7 +12,7 @@ with open(file) as fp:
         soup = BeautifulSoup(html_page, "lxml")
         links = []
         print("\n")
-        print(line.strip())
+        print(line.strip() + "\n")
         for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
             links.append(link.get('href'))
         for link in links:
@@ -25,8 +22,8 @@ with open(file) as fp:
             soup = BeautifulSoup(html_page, "lxml")
             #soup = html_page.read().decode('utf-8')
             #print("\n")
-            if(len(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", str(soup))) > 0):
-                for i in set(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", str(soup))):
+            if(len(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.(?!png|gif)[A-Za-z]{2,4}", str(soup))) > 0):
+                for i in set(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.(?!png|gif)[A-Za-z]{2,4}", str(soup))):
                     print(i)
 
         line = fp.readline()
